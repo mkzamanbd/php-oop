d4be9d20e71e356fd76724b5ded1feae861ac6f4
Kamruzzaman <zamandotbd@gmail.com>
2022-06-01 21:00:09 +0600

    added base class

--- app/Animal.php
+++ app/Animal.php
@@ -4,5 +4,8 @@ namespace App;
 
 interface Animal
 {
+    /**
+     * @return mixed
+     */
     public function makeSound();
 }
--- app/Car.php
+++ app/Car.php
@@ -4,12 +4,21 @@ namespace App;
 
 abstract class Car
 {
+    /**
+     * @var $name
+     */
     public $name;
 
+    /**
+     * @param $name
+     */
     public function __construct($name)
     {
         $this->name = $name;
     }
 
+    /**
+     * @return string
+     */
     abstract public function intro(): string;
 }
